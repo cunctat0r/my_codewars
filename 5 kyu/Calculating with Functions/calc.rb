e5555255e8d6976194f9e65864d535ce4668474c
Slava Petrov <slavik--petrov@yandex.ru>
2016-04-08 11:54:27 +0300

    Calculating with functions

---
+++ 5 kyu/Calculating with Functions/calc.rb
@@ -0,0 +1,52 @@
+def zero(args=nil)
+  return 0 if args.nil?
+  return eval "0#{args}"
+end
+def one(args=nil)
+  return 1 if args.nil?
+  return eval "1#{args}"
+end
+def two(args=nil)
+  return 2 if args.nil?
+  return eval "2#{args}"
+end
+def three(args=nil)
+  return 3 if args.nil?
+  return eval "3#{args}"
+end
+def four(args=nil)
+  return 4 if args.nil?
+  return eval "4#{args}"
+end
+def five(args=nil)
+  return 5 if args.nil?
+  return eval "5#{args}"
+end
+def six(args=nil)
+  return 6 if args.nil?
+  return eval "6#{args}"
+end
+def seven(args=nil)
+  return 7 if args.nil?
+  return eval "7#{args}"
+end
+def eight(args=nil)
+  return 8 if args.nil?
+  return eval "8#{args}"
+end
+def nine(args=nil)
+  return 9 if args.nil?
+  return eval "9#{args}"
+end
+def plus(args=nil)
+  return "+#{args}"
+end
+def minus(args=nil)
+  return "-#{args}"
+end
+def times(args=nil)
+  return "*#{args}"
+end
+def divided_by(args=nil)
+  return "/#{args}.0"
+end
